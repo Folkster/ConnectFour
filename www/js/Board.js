193c36389228eb1bec2a07911e90c0ed60d21fc3
Peter Sjögren <peter.sjogren@live.se>
2018-12-10 20:24:49 +0100

    Slot position test
    Console log method

--- www/js/Board.js
+++ www/js/Board.js
@@ -3,14 +3,21 @@ class Board extends Component {
         super();
         this.addRoute('/board', 'Connect 4 Game');
         this.colArray = [];
+        this.slotNbr = 1;
         this.createCols();
+
     }
 
     createCols() {
         for (let x = 0; x <= 6; x++) {
             this.colArray[x] = new Col(this, x);            
         }
-        console.log(this.colArray);
+        for (let i = 0; i<this.colArray.length; i++){
+            for (let t = 0; t<this.colArray[i].slots.length; t++){
+                console.log("Slot " + this.slotNbr + " X:" + this.colArray[i].slots[t].x + " Y:" + this.colArray[i].slots[t].y);
+                this.slotNbr++;
+            }
+        }
         
     }
 }
