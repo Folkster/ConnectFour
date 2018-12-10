1bf59c4017d3e495eba72d940b2e5974526f3508
Anna <zoora1234@hotmail.com>
2018-12-10 10:59:27 +0100

    Hiscore - och senaste versionen av programmet

--- www/js/NavBar.js
+++ www/js/NavBar.js
@@ -4,20 +4,16 @@ class NavBar extends Component {
     super();
     this.navItems = [
       new NavItem('Start', '/'),
-<<<<<<< Updated upstream
       new NavItem('Play Game', '/play'),
       new NavItem('About', '/about'),
-      new NavItem('Rules', '/rules')
+      new NavItem('Rules', '/rules'),
+      new NavItem('HiScore', '/hiScore')
     ];
     this.playingNav = [
       new NavItem('-', '/'),
       new NavItem('Exit Game', '/'),
       new NavItem('-', '/'),
       new NavItem('-', '/')
-=======
-      new NavItem('About', '/about'),
-      new NavItem('HiScore', '/hiScore')
->>>>>>> Stashed changes
     ];
   }
 
