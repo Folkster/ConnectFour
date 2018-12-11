d7fdfb26f41ae7a4da4ca3374e4e79c20ed6f458
Peter Sjögren <peter.sjogren@live.se>
2018-12-11 12:20:03 +0100

    Change slot color test

--- www/js/Arrow.js
+++ www/js/Arrow.js
@@ -9,12 +9,10 @@ class Arrow extends Component{
 
     arrowClick() {
         
-        for(let i = 0; i<this.col.slots.length;i++){
+        for(let i = this.col.slots.length-1; i=>0 ;i--){
             if(this.col.slots[i].isEmpty){
                 console.log(this.col.slots[i]);
-/*                 function colorchange() {
-                    this.col.slots[i].style.color = "red";
-                } */
+                document.getElementById("slot-"+this.col.slots[i].x+this.col.slots[i].y).style.color = "red";
                 this.col.slots[i].isEmpty = false;
                 break;
             }
