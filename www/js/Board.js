190e915247ca6a6aca6781c8e6dd72ea13028410
Peter Sjögren <peter.sjogren@live.se>
2018-12-17 12:22:26 +0100

    Board matrix fix

--- www/js/Board.js
+++ www/js/Board.js
@@ -3,7 +3,6 @@ class Board extends Component {
         super();
         this.addRoute('/board', 'Connect 4 Game');
         this.colArray = [];
-        this.slotNbr = 1;
         this.players = thePlayers;
         this.createCols();
         this.game = new Game(this);
@@ -13,11 +12,7 @@ class Board extends Component {
         for (let x = 0; x <= 6; x++) {
             this.colArray[x] = new Col(this, x);
         }
-        for (let i = 0; i < this.colArray.length; i++) {
-            for (let t = 0; t < this.colArray[i].slots.length; t++) {
-                this.slotNbr++;
-            }
-        }
+        
     }
 
 }
