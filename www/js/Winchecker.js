9caeec5b39d388c07b4b778d573f5a16c7810249
Peter Sjögren <peter.sjogren@live.se>
2018-12-18 15:44:30 +0100

    Collect win-slot & test-log

--- www/js/Winchecker.js
+++ www/js/Winchecker.js
@@ -6,6 +6,7 @@ class Winchecker {
         this.yPos;
         this.color;
         this.counter;
+        this.winningSlots = [];
     }
 
     check(slot) {
@@ -13,20 +14,24 @@ class Winchecker {
         this.yPos = slot.y;
         this.color = slot.color;
 
+        this.winningSlots.push(slot);
         this.checkHorizontal();
         this.winCheck();
 
         if (this.counter !== 0 && slot.y >= 3) {
+            this.winningSlots.push(slot);
             this.checkVertical();
             this.winCheck();
         }
 
         if (this.counter !== 0) {
+            this.winningSlots.push(slot);
             this.checkUpRightDownLeft();
             this.winCheck();
         }
 
         if (this.counter !== 0) {
+            this.winningSlots.push(slot);
             this.checkUpLeftDownRight();
             this.winCheck();
         }
@@ -47,6 +52,7 @@ class Winchecker {
         for (let i = this.xPos + 1; i <= 6; i++) {
             if (this.color === App.game.board.colArray[(i)].slots[this.yPos].color) {
                 this.counter++;
+                this.winningSlots.push(App.game.board.colArray[(i)].slots[this.yPos])
             } else break;
         }
     }
@@ -56,16 +62,18 @@ class Winchecker {
         for (let i = this.xPos - 1; i >= 0; i--) {
             if (this.color === App.game.board.colArray[(i)].slots[this.yPos].color) {
                 this.counter++;
+                this.winningSlots.push(App.game.board.colArray[(i)].slots[this.yPos])
             } else break;
         }
     }
 
     checkVertical() {
-        this.counter = 0;
+        this.counter = 1;
         console.log('CHECKING VERTICAL');
-        for (let i = this.yPos; i >= 0; i--) {
+        for (let i = this.yPos-1; i >= 0; i--) {
             if (this.color === App.game.board.colArray[(this.xPos)].slots[i].color) {
                 this.counter++;
+                this.winningSlots.push(App.game.board.colArray[(this.xPos)].slots[i]);
             } else {
                 break;
             }
@@ -83,9 +91,10 @@ class Winchecker {
         let horiz = this.xPos + 1;
         for (let i = this.yPos + 1; i <= 5; i++) {
             if (i >= 0 && i <= 5 && horiz >= 0 && horiz <= 6) {
-                if (this.color === App.game.board.colArray[(horiz++)].slots[i].color) {
+                if (this.color === App.game.board.colArray[(horiz)].slots[i].color) {
                     this.counter++;
-                    console.log(this.counter);
+                    this.winningSlots.push(App.game.board.colArray[(horiz)].slots[i]);
+                    horiz++;
                 } else break;
             } else break;
         }
@@ -96,9 +105,10 @@ class Winchecker {
         let horiz = this.xPos - 1;
         for (let i = this.yPos - 1; i >= 0; i--) {
             if (i >= 0 && i <= 5 && horiz >= 0 && horiz <= 6) {
-                if (this.color === App.game.board.colArray[(horiz--)].slots[i].color) {
+                if (this.color === App.game.board.colArray[(horiz)].slots[i].color) {
                     this.counter++;
-                    console.log(this.counter);
+                    this.winningSlots.push(App.game.board.colArray[(horiz)].slots[i]);
+                    horiz--;
                 } else break;
             } else break;
         }
@@ -115,9 +125,10 @@ class Winchecker {
         let horiz = this.xPos - 1;
         for (let i = this.yPos + 1; i <= 5; i++) {
             if (i >= 0 && i <= 5 && horiz >= 0 && horiz <= 6) {
-                if (this.color === App.game.board.colArray[(horiz--)].slots[i].color) {
+                if (this.color === App.game.board.colArray[(horiz)].slots[i].color) {
                     this.counter++;
-                    console.log(this.counter);
+                    this.winningSlots.push(App.game.board.colArray[(horiz)].slots[i]);
+                    horiz--;
                 } else break;
             } else break;
         }
@@ -128,9 +139,10 @@ class Winchecker {
         let horiz = this.xPos + 1;
         for (let i = this.yPos - 1; i >= 0; i--) {
             if (i >= 0 && i <= 5 && horiz >= 0 && horiz <= 6) {
-                if (this.color === App.game.board.colArray[(horiz++)].slots[i].color) {
+                if (this.color === App.game.board.colArray[(horiz)].slots[i].color) {
                     this.counter++;
-                    console.log(this.counter);
+                    this.winningSlots.push(App.game.board.colArray[(horiz)].slots[i]);
+                    horiz++;
                 } else break;
             } else break;
         }
@@ -140,15 +152,20 @@ class Winchecker {
         if (this.counter === 4) {
             if (this.color === 'red') {
                 console.log('WINNER: ' + App.game.board.players[0].name);
+                console.log(this.winningSlots);
+                
                 App.game.disableGameBoard();
                 App.game.displayWinner(this.board.players[0]);
 
             } else {
                 console.log('WINNER: ' + App.game.board.players[1].name);
+                console.log(this.winningSlots);
+
                 App.game.disableGameBoard();
                 App.game.displayWinner(App.game.board.players[1]);
             }
             this.counter = 0;
         }
+        this.winningSlots = [];
     }
 }
